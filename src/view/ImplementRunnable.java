a65fa8ecebd0b87b92e4df5433b0e35fe4f07f5e
GravesNathan <rgraves.math@gmail.com>
2017-09-27 19:32:52 -0500

    Runnable done

--- src/view/ImplementRunnable.java
+++ src/view/ImplementRunnable.java
@@ -9,18 +9,18 @@ package view;
  *
  * @author Nathan
  */
-public class MyRunnableThread implements Runnable{
+public class ImplementRunnable implements Runnable{
 
  
     public static int myCount = 0;
-    public MyRunnableThread(){
+    public ImplementRunnable(){
          
     }
     @Override
     public void run() {
-        while(MyRunnableThread.myCount <= 10){
+        while(ImplementRunnable.myCount <= 10){
             try{
-                System.out.println("Expl Thread: "+(++MyRunnableThread.myCount));
+                System.out.println("Expl Thread: "+(++ImplementRunnable.myCount));
                 Thread.sleep(100);
             } catch (InterruptedException iex) {
                 System.out.println("Exception in thread: "+iex.getMessage());
